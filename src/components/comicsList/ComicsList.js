01e314039637bdb22b4d8fb32516ebb1b9df360f
DanielFilimonov <marseanen85@gmail.com>
2025-09-26 12:32:07 +0300

    make some changes

---
+++ src/components/comicsList/ComicsList.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import useMarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMesage";
+
+import "./comicsList.scss";
+
+const ComicsList = () => {
+	const [comicList, setComicList] = useState([]);
+	const [newItemLoading, setNewItemLoading] = useState(false);
+	const [offset, setOffset] = useState(0);
+	const [comicEnded, setComicEnded] = useState(false);
+
+	const { loading, error, getAllComics } = useMarvelService();
+
+	useEffect(() => {
+		onRequest(offset, true);
+	}, []);
+
+	const onRequest = (offset, initial) => {
+		initial ? setNewItemLoading(false) : setNewItemLoading(true);
+		getAllComics(offset).then(onComicListLoaded);
+	};
+
+	const onComicListLoaded = (newComicList) => {
+		let ended = false;
+
+		if (newComicList.length < 8) {
+			ended = true;
+		}
+
+		setComicList([...comicList, ...newComicList]);
+		setNewItemLoading(false);
+		setOffset(offset + 8);
+		setComicEnded(ended);
+	};
+
+	function renderItems(arr) {
+		const items = arr.map((item, i) => {
+			return (
+				<li className="comics__item" key={i}>
+					<Link to={`/comics/${item.id}`}>
+						<img
+							src={item.thumbnail}
+							alt={item.title}
+							className="comics__item-img"
+						/>
+						<div className="comics__item-name">{item.title}</div>
+						<div className="comics__item-price">{item.price}</div>
+					</Link>
+				</li>
+			);
+		});
+		return <ul className="comics__grid">{items}</ul>;
+	}
+
+	const items = renderItems(comicList);
+
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading && !newItemLoading ? <Spinner /> : null;
+
+	return (
+		<div className="comics__list">
+			{errorMessage}
+			{spinner}
+			{items}
+			<button
+				disabled={newItemLoading}
+				style={{ display: comicEnded ? "none" : "block" }}
+				onClick={() => onRequest(offset)}
+				className="button button__main button__long"
+			>
+				<div className="inner">load more</div>
+			</button>
+		</div>
+	);
+};
+
+export default ComicsList;
