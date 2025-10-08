aa77d22ac1b1a3b5ca7233571dc4457384831c37
DanielFilimonov <marseanen85@gmail.com>
2025-10-08 13:20:41 +0300

    before state machine

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,40 +1,39 @@
 import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import Spinner from "../spinner/Spinner";
-import ErrorMessage from "../errorMessage/ErrorMesage";
 import useMarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from '../errorMessage/ErrorMesage';
+
 import "./charList.scss";
 
 const CharList = (props) => {
 	const [charList, setCharList] = useState([]);
-	const [newItemLoading, setNewItemLoading] = useState(false);
-	const [offset, setOffset] = useState(219);
+	const [newItemLoading, setnewItemLoading] = useState(false);
+	const [offset, setOffset] = useState(210);
 	const [charEnded, setCharEnded] = useState(false);
 
-	const {loading, error, getAllCharacters} = useMarvelService();
+	const { loading, error, getAllCharacters } = useMarvelService();
 
 	useEffect(() => {
 		onRequest(offset, true);
 	}, []);
 
 	const onRequest = (offset, initial) => {
-		initial ? setNewItemLoading(false) : setNewItemLoading(true);
-			getAllCharacters(offset)
-			.then(onCharListLoaded)
+		initial ? setnewItemLoading(false) : setnewItemLoading(true);
+		getAllCharacters(offset).then(onCharListLoaded);
 	};
 
-
-	const onCharListLoaded = (newCharList) => {
+	const onCharListLoaded = async (newCharList) => {
 		let ended = false;
 		if (newCharList.length < 9) {
 			ended = true;
 		}
-
-		setCharList((charList) => [...charList, ...newCharList]);
-		setNewItemLoading((newItemLoading) => false);
-		setOffset((offset) => offset + 9);
-		setCharEnded((charEnded) => ended);
+		setCharList([...charList, ...newCharList]);
+		setnewItemLoading(false);
+		setOffset(offset + 9);
+		setCharEnded(ended);
 	};
 
 	const itemRefs = useRef([]);
@@ -58,33 +57,42 @@ const CharList = (props) => {
 			}
 
 			return (
-				<li
-					className="char__item"
-					tabIndex={0}
-					ref={(el) => (itemRefs.current[i] = el)}
+				<CSSTransition
 					key={item.id}
-					onClick={() => {
-						props.onCharSelected(item.id);
-						focusOnItem(i);
-					}}
-					onKeyPress={(e) => {
-						if (e.key === " " || e.key === "Enter") {
+					timeout={500}
+					classNames="char__item"
+				>
+					<li
+						className="char__item"
+						tabIndex={0}
+						ref={(el) => (itemRefs.current[i] = el)}
+						onClick={() => {
 							props.onCharSelected(item.id);
 							focusOnItem(i);
-						}
-					}}
-				>
-					<img
-						src={item.thumbnail}
-						alt={item.name}
-						style={imgStyle}
-					/>
-					<div className="char__name">{item.name}</div>
-				</li>
+						}}
+						onKeyPress={(e) => {
+							if (e.key === " " || e.key === "Enter") {
+								props.onCharSelected(item.id);
+								focusOnItem(i);
+							}
+						}}
+					>
+						<img
+							src={item.thumbnail}
+							alt={item.name}
+							style={imgStyle}
+						/>
+						<div className="char__name">{item.name}</div>
+					</li>
+				</CSSTransition>
 			);
-        });
-        
-		return <ul className="char__grid">{items}</ul>;
+		});
+
+		return (
+			<ul className="char__grid">
+				<TransitionGroup component={null}>{items}</TransitionGroup>
+			</ul>
+		);
 	}
 
 	const items = renderItems(charList);
@@ -92,16 +100,15 @@ const CharList = (props) => {
 	const errorMessage = error ? <ErrorMessage /> : null;
 	const spinner = loading && !newItemLoading ? <Spinner /> : null;
 
-
 	return (
 		<div className="char__list">
 			{errorMessage}
 			{spinner}
 			{items}
 			<button
-				className="button button__main button__long"
 				disabled={newItemLoading}
 				style={{ display: charEnded ? "none" : "block" }}
+				className="button button__main button__long"
 				onClick={() => onRequest(offset)}
 			>
 				<div className="inner">load more</div>
@@ -110,8 +117,8 @@ const CharList = (props) => {
 	);
 };
 
-CharList.prototype = {
-    onCharSelected: PropTypes.func.isRequired
-}
+CharList.propTypes = {
+	onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -15,6 +15,13 @@ const useMarvelService = () => {
         return res.data.results.map(_transformCharacter);
 	};
 
+		const getCharacterByName = async (name) => {
+			const res = await request(
+				`${_apiBase}characters?name=${name}&${_apiKey}`
+			);
+			return res.data.results.map(_transformCharacter);
+		};
+
 	const getCharacter = async (id) => {
 		const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
 		return _transformCharacter(res.data.results[0]);
@@ -70,6 +77,7 @@ const useMarvelService = () => {
 		clearError,
 		getAllComics,
 		getComic,
+		getCharacterByName,
 	};
 };
 
